67b387a3c0a1357e7386cb88a9281ff2b40d7bab
masonomara <mason.omara@gmail.com>
2026-04-25 09:16:27 -0600

    initial main setup

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-def main():
-    print("Hello from ux-swarm!")
+import click
+
+
+@click.command()
+@click.argument("target")
+@click.argument("task")
+def cli(target, task):
+    """Simulate a swarm of users completing TASK on TARGET.
+
+    TARGET can be a URL or a path to a screenshot.
+    TASK is a natural-language description of what users should complete.
+
+    Example: ux-swarm https://myapp.com "complete the checkout flow"
+    """
 
 
 if __name__ == "__main__":
-    main()
+    cli()
